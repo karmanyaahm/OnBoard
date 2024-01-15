c4bf9a6abe129437f65f2d625e40ebdd86227ff6
Karmanyaah Malhotra <karmanyaah.git@malhotra.cc>
2024-01-14 18:37:01 -0600

    test

--- .github/index.js
+++ .github/index.js
@@ -11,21 +11,25 @@ async function run({ gh, ctx }) {
 
 // make or update comment with `body` markdown
 async function comment(body) {
+
+ body = '<!-- MY-ONBOARD-BOT --> ' + body
  let id = await already();
   if (id === -1) {
     github.rest.issues.createComment({
       issue_number: context.issue.number,
       owner: context.repo.owner,
       repo: context.repo.repo,
-      body: '<!-- MY-ONBOARD-BOT -->' + body,
+      body: body,
     })
+    console.log(`Creating new comment with ${body} on ${context.repo.owner}/${context.repo.repo}#${context.issue.number}`)
   } else {
      github.rest.issues.updateComment({
       comment_id: id,
       owner: context.repo.owner,
       repo: context.repo.repo,
-      body: '<!-- MY-ONBOARD-BOT -->' + body,
+      body: body,
     })
+    console.log(`Updating ${id} to ${body} on ${context.repo.owner}/${context.repo.repo}#${context.issue.number}`)
   }
 }
 
